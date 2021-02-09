c96ba540d651a364b5c15ebd8b6d48d881c3823e
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-02-08 21:30:19 -0300

    :up: Implementando função Contar

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,19 @@
 //Configurando função verificar, que será ativada quando ocorrer evento de click
 function contar(){
-      
+    //Variáveis que serão utilizadas
+    var inicio = Number(document.getElementById("txtini").value);
+    var fim = Number(document.getElementById("txtfim").value);
+    var passo = Number(document.getElementById("txtpasso").value);
+    var answer = document.getElementById("resposta");
+    //Iniciando While
+    while (1){
+      answer.innerText += `${inicio} -->`
+      if (inicio >= fim)
+      {
+        answer.innerText += `Fim`
+        break;
+      }
+      inicio += passo;
+    }
+
 }
